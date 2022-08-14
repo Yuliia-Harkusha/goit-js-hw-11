cddfdf83af9fa5692161cd00b0cf8944621f38e0
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-08-14 23:21:16 +0200

    add libraries

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './css/styles.css';
+import axios from "axios";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const refs = {
+    form: document.querySelector('.search-form'),
+    input: document.querySelector('input'),
+    gallery: document.querySelector('.gallery'),
+};
+
