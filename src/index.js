ff695441a234d034aa118495e8949edded12a94a
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-08-16 18:25:51 +0200

    initialize lightbox

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ const refs = {
     gallery: document.querySelector('.gallery'),
 };
 
+const lightbox = new SimpleLightbox('.photo-card a', {
+    captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
 function renderCard(arr) {
     const markupCard = arr.map(item => markup(item)).join('');
     refs.gallery.insertAdjacentHTML('beforeend', markupCard);
@@ -37,21 +45,20 @@ async function onSubmitForm(e) {
         if (response.totalHits > 0) {
             Notify.success(`Hooray! We found ${response.totalHits} images.`);
             renderCard(response.hits);
+            lightbox.refresh();
 
-            // const { height: cardHeight } = document
-            // .querySelector(".gallery")
-            // .firstElementChild.getBoundingClientRect();
+            const { height: cardHeight } = document
+            .querySelector(".gallery")
+            .firstElementChild.getBoundingClientRect();
 
-            // window.scrollBy({
-            // top: cardHeight * 2,
-            // behavior: "smooth",
-            // });
+            window.scrollBy({
+            top: cardHeight * 2,
+            behavior: "smooth",
+            });
         }
-
     } catch (error) {
         console.log(error);
     };
-
 };
 
 refs.form.addEventListener('submit', onSubmitForm);
