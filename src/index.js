540dc30c41d1a39031fdfac07f1982a3227df8ef
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-08-18 19:18:26 +0200

    add scroll

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import throttle from 'lodash.throttle';
 import { markup } from "./render";
 import { getPhotos } from './pixabay';
 
@@ -62,4 +63,32 @@ async function onSubmitForm(e) {
 };
 
 refs.form.addEventListener('submit', onSubmitForm);
+
+
+window.addEventListener('scroll', async () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	
+    if (clientHeight + scrollTop >= scrollHeight - 5) {
+        const query = refs.input.value.trim();
+        const response = await getPhotos(query);
+
+        try {
+            renderCard(response.hits);
+            lightbox.refresh();
+
+            const { height: cardHeight } = document
+                .querySelector(".gallery")
+                .firstElementChild.getBoundingClientRect();
+
+            window.scrollBy({
+                top: cardHeight * 2,
+                behavior: "smooth",
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+}
+);
+
 
